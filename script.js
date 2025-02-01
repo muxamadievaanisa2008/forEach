f5a063adf93d95b32ee1a434819f340819080a0d
Anisa <anisamuxamadieva@icloud.com>
2025-02-01 13:14:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// ДЗ1:
+let nums = [1, 2, 5, 6, 22, 44, 66, 3, 4, 17, 22, 444];
+let sum = 0;
+
+nums.forEach(number => {sum += number;});
+
+console.log("Равно:", sum); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
